eaf5fb65cdaa62762f81a221d566233e535367ee
Louis König <louis115@gmx.de>
2022-02-02 09:56:14 +0100

    basic Design of Login Screen

--- js/tabs/HomeScreen.js
+++ js/tabs/HomeScreen.js
@@ -1,13 +1,79 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import {
-    SafeAreaView,
-    Text
+    StyleSheet,
+    View,
+    Text,
+    Button,
+    TextInput
 }
     from 'react-native';
 
 export default function HomeScreen() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const clickLogin = () => {
+        console.log("Test");
+    }
+
   return (
-      <SafeAreaView>
-          <Text>Login</Text>
-      </SafeAreaView>);
+      <View style={styles.container}>
+
+          {/*Title*/}
+          <Text style={[styles.title, styles.field]}>Login</Text>
+
+          {/*Email*/}
+          <View style={styles.field}>
+              <Text style={styles.inputHint}>E-Mail</Text>
+              <TextInput value={email}
+                         onChangeText={setEmail}
+                         style={styles.input}/>
+          </View>
+
+          {/*Password*/}
+          <View style={styles.field}>
+              <Text style={styles.inputHint}>Password</Text>
+              <TextInput value={password}
+                         onChangeText={setPassword}
+                         style={styles.input}/>
+          </View>
+
+          {/* Styles not working */}
+          <Button title="Button"
+                  onPress={() => clickLogin()}
+                  style={[styles.button, styles.field]}/>
+      </View>);
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        fontWeight: "600"
+    },
+    button: {
+        backgroundColor: "purple"
+    },
+    field: {
+        paddingTop: 20
+
+    },
+    inputHint: {
+        paddingLeft: 3,
+        paddingBottom: 5
+    },
+    input: {
+        fontSize: 18,
+        height: 45,
+        padding: 10,
+        borderRadius: 3,
+        backgroundColor: "white",
+
+    },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        paddingTop: 20,
+        paddingHorizontal: 20
+    }
+});
